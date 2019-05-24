d1b4611a5d613bb0208e224b83455540c2eed96c
Gustav E Gennrich <ggennrich@gmail.com>
2019-05-23 20:05:00 -0700

    add option to only show the href

--- app/helpers/shipit/stacks_helper.rb
+++ app/helpers/shipit/stacks_helper.rb
@@ -66,13 +66,17 @@ module Shipit
       end
     end
 
-    def pull_request_link(pull_request_or_commit)
+    def pull_request_link(pull_request_or_commit, only_href=false)
       number = if pull_request_or_commit.respond_to?(:pull_request_number)
         pull_request_or_commit.pull_request_number
       else
         pull_request_or_commit.number
       end
-      link_to("##{number}", github_pull_request_url(pull_request_or_commit), target: '_blank', class: 'number')
+      if only_href
+        github_pull_request_url(pull_request_or_commit)
+      else
+        link_to("##{number}", github_pull_request_url(pull_request_or_commit), target: '_blank', class: 'number')
+      end
     end
 
     def render_raw_commit_id_link(commit)
