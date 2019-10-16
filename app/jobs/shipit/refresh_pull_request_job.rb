323d335bae6b7657ee2a96fbad19db933ed29811
Gustav E Gennrich <ggennrich@gmail.com>
2019-10-16 14:24:01 -0700

    add delete functionality

--- app/jobs/shipit/refresh_pull_request_job.rb
+++ app/jobs/shipit/refresh_pull_request_job.rb
@@ -3,6 +3,12 @@ module Shipit
     queue_as :default
 
     def perform(pull_request, force_merge=false)
+      return if pull_request&.id.nil?
+
+      pull_request = PullRequest.where(id: pull_request.id).first
+
+      return if pull_request.nil?
+
       pull_request.refresh!
 
       if force_merge
