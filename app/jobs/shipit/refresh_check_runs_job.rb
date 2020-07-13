d8f8c18e540a4409d172a026f1f533c7c258ec58
Gustav E Gennrich <ggennrich@gmail.com>
2020-07-13 15:33:11 -0700

    set defaults

--- app/jobs/shipit/refresh_check_runs_job.rb
+++ app/jobs/shipit/refresh_check_runs_job.rb
@@ -3,7 +3,7 @@ module Shipit
     include Sidekiq::Worker
     sidekiq_options lock: :until_and_while_executing, queue: 'default'
 
-    def perform(stack_id, commit_id)
+    def perform(stack_id=nil, commit_id=nil)
       if commit_id
         Commit.find(commit_id).refresh_check_runs!
       else
